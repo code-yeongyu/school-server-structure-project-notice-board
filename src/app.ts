1595a871fe219deb9a5656be41fa25165c0448a1
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-12-02 01:06:51 +0900

    Fix unused import in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express"
 import morgan from "morgan"
 import bodyParser from "body-parser"
-import passport from "passport"
 import routes from "routes"
 
 const app = express()
@@ -11,7 +10,6 @@ if (process.env.NODE_ENV !== "test") {
     app.use(morgan("combined"))
 }
 
-app.use(passport.initialize())
 app.use("", routes)
 
 export default app
