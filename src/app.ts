d7fea5402bcd60491679d87448113a9d0e5a236c
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-12-02 00:39:37 +0900

    Update codes for the school project

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,7 @@ import express from "express"
 import morgan from "morgan"
 import bodyParser from "body-parser"
 import passport from "passport"
-import passportConfig from "config/passport"
 import routes from "routes"
-
-passportConfig()
 
 const app = express()
 app.use(bodyParser.json())
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
-import userRouter from 'app/user/routes'
-import { Router } from 'express'
+import articleRouter from "app/article/routes"
+import { Router } from "express"
 
 const router = Router()
 
-router.use('/user', userRouter)
+router.use("/article", articleRouter)
 
 export default router
